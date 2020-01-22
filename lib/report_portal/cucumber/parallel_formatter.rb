7d55b1d1a4c570b737e4a15f3846e5daf7eb30b4
Sandeep Singh <ssingh31@massmutual.com>
2020-01-22 13:27:34 -0500

    Resolving merge conflicts after merging with master branch

--- lib/report_portal/cucumber/parallel_formatter.rb
+++ lib/report_portal/cucumber/parallel_formatter.rb
@@ -4,10 +4,16 @@ require_relative 'parallel_report'
 module ReportPortal
   module Cucumber
     class ParallelFormatter < Formatter
+      # @api private
+      def initialize(config)
+        @start_launch_time = ReportPortal.now
+        super(config)
+      end
+
       private
 
       def report
-        @report ||= ReportPortal::Cucumber::ParallelReport.new
+        @report = ReportPortal::Cucumber::ParallelReport.new(@start_launch_time)
       end
     end
   end
